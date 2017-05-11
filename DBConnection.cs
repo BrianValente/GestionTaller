b225217b257feefc4f41c7abc8a6935aef54182d
Brian Valente <briannvalente@gmail.com>
2017-05-11 17:35:23 -0300

    Incluir SQLite #2

--- DBConnection.cs
+++ DBConnection.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
 
 namespace Gestion_Taller {
     public sealed class DBConnection {
         private static readonly DBConnection instance = new DBConnection();
+        private SQLiteConnection sqliteConnection;
+        private static String dbFileName = "database.sqlite";
 
         private DBConnection() {
 
